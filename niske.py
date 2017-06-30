db3e90a0504cdcce44f449d33c51b1d58ea37111
helenasavic <helena.savic22@gmail.com>
2017-06-30 16:24:49 +0200

    Add files via upload

---
+++ niske.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+x='Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: "Ju, Mitre, uplašio si me!". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica.'
+listareci=x.split(' ')
+print(listareci)
+
+
+m="Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: \"Ju, Mitre, uplašio si me!\". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica."
+m = m.lower()
+print(m)
+listareci=m.split(" ")
+print(listareci)
+
+niska = 'Dokle cu vise biti niska!'
+karakteri = list(niska)
+print(karakteri)
+
+broj = '5'
+print(int(broj)+10)
