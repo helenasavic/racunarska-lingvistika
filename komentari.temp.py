2b3bc7fa0fd62d7bd47bdc7ee6a72d5a2a94c01a
helenasavic <helena.savic22@gmail.com>
2017-06-30 13:53:39 +0200

    Add files via upload

---
+++ komentari.temp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+print("Hello world!")
+
+""""ovo je komentar
+ovo je i dalje komentar
+ovo je i dalje komentar
+"""
+
+print("ovo je druga linija")
+print("ovo je racunarska lingvistika")
